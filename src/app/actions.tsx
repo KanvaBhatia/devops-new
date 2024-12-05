d7b13e3a2c8fe75ee661c62ef7f3b63b4ef713f9
Kanva Bhatia <kanva.bhatia@gmail.com>
2024-12-05 20:30:33 +0530

    github flow added

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -1,22 +1,23 @@
-'use server';
+"use server";
 
-import { streamUI } from 'ai/rsc';
-import { createOpenAI } from '@ai-sdk/openai';
-import { z } from 'zod';
+import { streamUI } from "ai/rsc";
+import { createOpenAI } from "@ai-sdk/openai";
+import { z } from "zod";
+import axios from "axios";
+var gh = require("parse-github-url");
 
-const openai = createOpenAI(
-    {
-        baseURL: "https://openrouter.ai/api/v1",
-        apiKey: "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"
-    }
-)
+const openai = createOpenAI({
+  baseURL: "https://openrouter.ai/api/v1",
+  apiKey:
+    "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",
+});
 const LoadingComponent = () => (
   <div className="animate-pulse p-4">getting weather...</div>
 );
 
 const getWeather = async (location: string) => {
-  await new Promise(resolve => setTimeout(resolve, 2000));
-  return '82°F️ ☀️';
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  return "82°F️ ☀️";
 };
 
 interface WeatherProps {
@@ -30,14 +31,52 @@ const WeatherComponent = (props: WeatherProps) => (
   </div>
 );
 
+const parseGitHubUrl = (url: string) => {
+  var repo_details = gh(url);
+  if (
+    repo_details == null ||
+    repo_details.owner == null ||
+    repo_details.name == null
+  ) {
+    return { error: "Invalid URL" };
+  }
+  return repo_details;
+};
+
 export async function streamComponent(link: string) {
+  const repo_details = parseGitHubUrl(link);
+  if (repo_details.error) {
+    return <div>{repo_details.error}</div>;
+  }
+  const gitResponse = await axios.get(
+    `https://api.github.com/repos/${repo_details.owner}/${repo_details.name}`,
+    {
+      headers: {
+        Authorization: `token ${process.env.GITHUB_TOKEN}`,
+      },
+    },
+  );
+
+  const { default_branch } = gitResponse.data;
+  console.log({ default_branch });
+  const defaultGitUrl = `https://api.github.com/repos/${repo_details.owner}/${repo_details.name}/git/trees/${default_branch}`
+  
+  const gitData = await axios.get(defaultGitUrl, {
+    headers: {
+      Authorization: `token ${process.env.GITHUB_TOKEN}`,
+    },
+  });
+
+  const nodes = gitData?.data?.tree
+  console.log({nodes})
+
   const result = await streamUI({
-    model: openai('gpt-4o'),
-    prompt: 'Get the weather for San Francisco',
+    model: openai("gpt-4o"),
+    prompt: "Get the weather for San Francisco",
     text: ({ content }) => <div>{content}</div>,
     tools: {
       getWeather: {
-        description: 'Get the weather for a location',
+        description: "Get the weather for a location",
         parameters: z.object({
           location: z.string(),
         }),
